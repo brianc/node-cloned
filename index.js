67ce41172ee82b1d70e2ade9e6ae1feab6272011
brianc <brian.m.carlson@gmail.com>
2013-03-06 16:02:55 -0600

    change name to cloned

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ var path = require('path');
 var exec = require('child_process').exec;
 var os = require('os');
 
-var requireSha = module.exports = function(workingDir, sha, cb) {
-  if(typeof sha == 'function') {
-    cb = sha;
-    sha = workingDir;
-    workingDir = os.tmpDir();
-  }
-  var moduleDir = path.join(workingDir, sha);
+var cloned = module.exports = function(sha, cb) {
+  var moduleDir = path.join(cloned.workingDir, sha);
   var options = { cwd: moduleDir };
   var clean = function(cb) {
     require('rmdir')(moduleDir, cb);
@@ -25,9 +20,11 @@ var requireSha = module.exports = function(workingDir, sha, cb) {
   });
 };
 
+cloned.workingDir = require('os').tmpDir();
+
 //returns the current sha of the module
 //only used in testing
-requireSha._getCurrentSha = function(cb) {
+cloned._getCurrentSha = function(cb) {
   exec('git log --max-count=1 --format=%h', {cwd: __dirname}, function(err, stdout) {
     if(err) return cb(err);
     return cb(null, stdout.trim());
--- test/index.js
+++ test/index.js
@@ -1,18 +1,21 @@
 var fs = require('fs');
 var assert = require('assert');
 
-var requireSha = require(__dirname + '/../')
-
+var cloned = require(__dirname + '/../')
 var workingDir = __dirname + '/.working';
+
+cloned.workingDir = workingDir;
+
 describe('require-sha', function() {
   after(function(done) {
-    require('rmdir')(workingDir, done);
+    require('rmdir')(cloned.workingDir, done);
   });
+
   it('works', function(done) {
-    requireSha(workingDir, 'd1a57ed', function(err, module) {
+    cloned('d1a57ed', function(err, module) {
       if(err) return done(err);
       assert.equal(typeof module.CURRENT_SHA, "undefined")
-      requireSha(workingDir, '183bfd9', function(err, repoDir, clean) {
+      cloned('183bfd9', function(err, repoDir, clean) {
         if(err) return done(err);
         var module = require(repoDir);
         assert.equal(module.CURRENT_SHA, 'd1a57ed');
@@ -27,16 +30,11 @@ describe('require-sha', function() {
     });
   });
 
-  it('does not require working dir', function(done) {
+  it('supports installing modules', function(done) {
     var wantedSha = 'aa9e877';
-    requireSha(wantedSha, function(err, repoDir, clean) {
-      var module = require(repoDir);
-      if(err) {
-        return clean(function() {
-          done(err);
-        });
-      }
-      var modulerequire
+    cloned(wantedSha, function(err, dir, clean) {
+      assert(fs.existsSync(workingDir + '/aa9e877/node_modules/rmdir'));
+      var module = require(dir);
       module._getCurrentSha(function(err, sha) {
         assert.equal(sha, wantedSha);
         clean(done);
@@ -44,16 +42,8 @@ describe('require-sha', function() {
     });
   });
 
-  it('supports installing modules', function(done) {
-    var wantedSha = 'aa9e877';
-    requireSha(workingDir, wantedSha, function(err, module, clean) {
-      assert(fs.existsSync(workingDir + '/aa9e877/node_modules/rmdir'));
-      clean(done);
-    });
-  });
-
   it('returns clean method even if there is an error', function(done) {
-    requireSha('asld', function(err, module, clean) {
+    cloned('asld', function(err, module, clean) {
       assert(err);
       assert.equal(module, null);
       assert.equal(typeof clean, 'function');
